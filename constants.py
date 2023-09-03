f12feacbd6fc3f218b14a9fe03ba89d38ecbef0b
jazziiRed <jazziiRedmc@gmail.com>
2023-09-02 17:43:58 -0700

    moved editable settings to top

--- constants.py
+++ constants.py
@@ -1,3 +1,54 @@
+# EDITABLE SETTING
+# Choose whether or not to use named discs
+# Using unnamed discs will reduce schematic size and
+# Possibly reduce lag due to less NBT data
+NAME_DISCS = 0,
+
+# EDITABLE SETTING
+# The number of full modules (2 half-modules) of each instrument
+# in the physical machine. Can be set to 0 if the module does not exist.
+# NOTE: These settings are configured for the machine in the world download.
+CHORD_MAX_SIZES = {
+    'piano': 3,
+    'double bass': 1,
+    'bass drum': 1,
+    'snare drum': 1,
+    'click': 1,
+    'guitar': 3,
+    'flute': 3,
+    'bell': 1,
+    'chime': 1,
+    'xylophone': 1,
+    'iron xylophone': 1,
+    'cow bell': 1,
+    'digeridoo': 1,
+    'bit': 1,
+    'banjo': 1,
+    'pling': 3,
+}
+
+# EDITABLE SETTING
+# Whether you want to keep the lowest or highest notes in chords that are too large
+# 'l' means keep the low notes; 'h' means keep the high notes.
+KEEP_NOTES_BY_INSTRUMENT = {
+    'piano': 'h',
+    'double bass': 'l',
+    'bass drum': 'l',
+    'snare drum': 'l',
+    'click': 'l',
+    'guitar': 'l',
+    'flute': 'h',
+    'bell': 'l',
+    'chime': 'l',
+    'xylophone': 'l',
+    'iron xylophone': 'l',
+    'cow bell': 'l',
+    'digeridoo': 'l',
+    'bit': 'l',
+    'banjo': 'l',
+    'pling': 'h',
+}
+
 # DO NOT EDIT
 # The maximum length of a song, dictated by the capacity of a double chest
 MAX_SONG_LENGTH = 1457
@@ -66,54 +117,3 @@ NOTES_TO_DISCS_NAMED = [
     '"minecraft:music_disc_wait",tag:{display:{Name:\'{"text":"F"}\'}}',
     '"minecraft:music_disc_pigstep",tag:{display:{Name:\'{"text":"F#"}\'}}',
 ]
-
-# EDITABLE SETTING
-# Choose whether or not to use named discs
-# Using unnamed discs will reduce schematic size and
-# Possibly reduce lag due to less NBT data
-NAME_DISCS = 0,
-
-# EDITABLE SETTING
-# The number of full modules (2 half-modules) of each instrument
-# in the physical machine. Can be set to 0 if the module does not exist.
-# NOTE: These settings are configured for the machine in the world download.
-CHORD_MAX_SIZES = {
-    'piano': 3,
-    'double bass': 1,
-    'bass drum': 1,
-    'snare drum': 1,
-    'click': 1,
-    'guitar': 3,
-    'flute': 3,
-    'bell': 1,
-    'chime': 1,
-    'xylophone': 1,
-    'iron xylophone': 1,
-    'cow bell': 1,
-    'digeridoo': 1,
-    'bit': 1,
-    'banjo': 1,
-    'pling': 3,
-}
-
-# EDITABLE SETTING
-# Whether you want to keep the lowest or highest notes in chords that are too large
-# 'l' means keep the low notes; 'h' means keep the high notes.
-KEEP_NOTES_BY_INSTRUMENT = {
-    'piano': 'h',
-    'double bass': 'l',
-    'bass drum': 'l',
-    'snare drum': 'l',
-    'click': 'l',
-    'guitar': 'l',
-    'flute': 'h',
-    'bell': 'l',
-    'chime': 'l',
-    'xylophone': 'l',
-    'iron xylophone': 'l',
-    'cow bell': 'l',
-    'digeridoo': 'l',
-    'bit': 'l',
-    'banjo': 'l',
-    'pling': 'h',
-}

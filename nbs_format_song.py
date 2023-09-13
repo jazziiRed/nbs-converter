45a33b71a321904d6c10698ec9db72aa4d18c1c1
IHasBone <89635515+IHasBone@users.noreply.github.com>
2023-09-13 17:42:48 +0200

    Went back to old naming scheme

--- nbs_format_song.py
+++ nbs_format_song.py
@@ -3,122 +3,122 @@ import sys
 from constants import *
 
 
-def get_valid_input(valid_inputs, prompt):
+def getValidInput(validInputs, prompt):
     while True:
-        user_input = input(prompt)
-        if user_input in valid_inputs:
-            return user_input
+        userInput = input(prompt)
+        if userInput in validInputs:
+            return userInput
         else:
-            print(f'"{user_input}" is not a valid input. Please try again.')
+            print(f'"{userInput}" is not a valid input. Please try again.')
 
 
-def remove_custom_notes(chord):
+def removeCustomNotes(chord):
     return [note for note in chord if note.instrument <= 15]
 
 
-def fix_illegal_notes(chord):
-    instrument_min, instrument_max = INSTRUMENT_RANGE
-    new_chord = []
+def fixIllegalNotes(chord):
+    instrumentMin, instrumentMax = INSTRUMENT_RANGE
+    newChord = []
 
     for note in chord:
-        while note.key < instrument_min:
+        while note.key < instrumentMin:
             note.key += 12
-        while note.key > instrument_max:
+        while note.key > instrumentMax:
             note.key -= 12
-        new_chord.append(note)
+        newChord.append(note)
 
-    return new_chord
+    return newChord
 
 
-def remove_highest_helper(chord, chord_max_size):
-    if len(chord) <= chord_max_size:
+def removeHighestHelper(chord, chordMaxSize):
+    if len(chord) <= chordMaxSize:
         return chord
 
-    highest_note = max(chord, key=lambda note: note.key)
-    chord.remove(highest_note)
+    highestNote = max(chord, key=lambda note: note.key)
+    chord.remove(highestNote)
 
-    return remove_highest_helper(chord, chord_max_size)
+    return removeHighestHelper(chord, chordMaxSize)
 
 
-def remove_highest_notes(chord, chord_max_size):
-    lower_octave_notes = upper_octave_notes = []
+def removeHighestNotes(chord, chordMaxSize):
+    lowerOctaveNotes = upperOctaveNotes = []
     for note in chord:
         if note.key < INSTRUMENT_RANGE[0] + 12:
-            lower_octave_notes.append(note)
+            lowerOctaveNotes.append(note)
         else:
-            upper_octave_notes.append(note)
+            upperOctaveNotes.append(note)
 
-    lower_octave_notes = remove_highest_helper(lower_octave_notes, chord_max_size)
-    upper_octave_notes = remove_highest_helper(upper_octave_notes, chord_max_size)
+    lowerOctaveNotes = removeHighestHelper(lowerOctaveNotes, chordMaxSize)
+    upperOctaveNotes = removeHighestHelper(upperOctaveNotes, chordMaxSize)
 
-    return lower_octave_notes + upper_octave_notes
+    return lowerOctaveNotes + upperOctaveNotes
 
 
-def remove_lowest_helper(chord, chord_max_size):
-    if len(chord) <= chord_max_size:
+def removeLowestHelper(chord, chordMaxSize):
+    if len(chord) <= chordMaxSize:
         return chord
 
-    lowest_note = min(chord, key=lambda note: note.key)
-    chord.remove(lowest_note)
+    lowestNote = min(chord, key=lambda note: note.key)
+    chord.remove(lowestNote)
 
-    return remove_lowest_helper(chord, chord_max_size)
-
-
-def remove_lowest_notes(chord, chord_max_size):
-    lower_octave_notes = [note for note in chord if note.key < INSTRUMENT_RANGE[0] + 12]
-    upper_octave_notes = [note for note in chord if note.key >= INSTRUMENT_RANGE[0] + 12]
-
-    lower_octave_notes = remove_lowest_helper(lower_octave_notes, chord_max_size)
-    upper_octave_notes = remove_lowest_helper(upper_octave_notes, chord_max_size)
-
-    return lower_octave_notes + upper_octave_notes
+    return removeLowestHelper(chord, chordMaxSize)
 
 
-def remove_chord_violations(chord):
-    list_of_chords = {}
+def removeLowestNotes(chord, chord_max_size):
+    lowerOctaveNotes = [note for note in chord if note.key < INSTRUMENT_RANGE[0] + 12]
+    upperOctaveNotes = [note for note in chord if note.key >= INSTRUMENT_RANGE[0] + 12]
+
+    lowerOctaveNotes = removeLowestHelper(lowerOctaveNotes, chord_max_size)
+    upperOctaveNotes = removeLowestHelper(upperOctaveNotes, chord_max_size)
+
+    return lowerOctaveNotes + upperOctaveNotes
+
+
+def removeChordViolations(chord):
+    listOfChords = {}
     for note in chord:
         instrument = note.instrument
-        if instrument in list_of_chords:
-            list_of_chords[instrument].append(note)
+        if instrument in listOfChords:
+            listOfChords[instrument].append(note)
         else:
-            list_of_chords[instrument] = [note]
+            listOfChords[instrument] = [note]
 
-    new_chord = []
-    for instrument, singleChord in list_of_chords.items():
-        max_size = CHORD_MAX_SIZES[INSTRUMENTS[instrument]]
+    newChord = []
+    for instrument, singleChord in listOfChords.items():
+        maxSize = CHORD_MAX_SIZES[INSTRUMENTS[instrument]]
 
         if KEEP_NOTES_BY_INSTRUMENT[INSTRUMENTS[instrument]] == 'h':
-            new_single_chord = remove_lowest_notes(singleChord, max_size)
+            newSingleChord = removeLowestNotes(singleChord, maxSize)
         else:
-            new_single_chord = remove_highest_notes(singleChord, max_size)
+            newSingleChord = removeHighestNotes(singleChord, maxSize)
 
-        new_chord.extend(new_single_chord)
+        newChord.extend(newSingleChord)
 
     # We need to preserve the original note order, because sometimes
     # saving has issues when notes are reordered
-    preserved_order_chord = [note for note in chord if note in new_chord]
+    preservedOrderChord = [note for note in chord if note in newChord]
 
-    return preserved_order_chord, len(preserved_order_chord) < len(chord)
+    return preservedOrderChord, len(preservedOrderChord) < len(chord)
 
 
-def main(song_file):
-    if not song_file.endswith('.nbs'):
+def main(songFile):
+    if not songFile.endswith('.nbs'):
         sys.exit('Your song file must end with ".nbs".')
 
     try:
-        song = pynbs.read(song_file)
-        song_name = song_file[:-4]
+        song = pynbs.read(songFile)
+        songName = songFile[:-4]
     except Exception as e:
-        sys.exit(f'An error occurred while reading the song file "{song_file}".\nError name: {e.__class__.__name__}\nExact error (search this up for help): {e}')
+        sys.exit(f'An error occurred while reading the song file "{songFile}".\nError name: {e.__class__.__name__}\nExact error (search this up for help): {e}')
 
     # give user option to compress song
-    original_song_length = song.header.song_length
-    if original_song_length > MAX_SONG_LENGTH:
-        print(f"Your song's length is {original_song_length}, and the max length of a song is {MAX_SONG_LENGTH}.")
+    originalSongLength = song.header.song_length
+    if originalSongLength > MAX_SONG_LENGTH:
+        print(f"Your song's length is {originalSongLength}, and the max length of a song is {MAX_SONG_LENGTH}.")
 
     print('You might want to compress your song if it is too slow or too long.')
     print('Compressing your song would remove every other tick and make it half as long. This may or may not make your song sound much worse.')
-    setting_compress = True if get_valid_input(['y', 'n'], 'Would you like to compress your song? (y/n): ') == 'y' else False
+    settingCompress = True if getValidInput(['y', 'n'], 'Would you like to compress your song? (y/n): ') == 'y' else False
 
     # warn user if there are notes out of range
     for note in song.notes:
@@ -132,43 +132,43 @@ def main(song_file):
         print('Your song contains custom instruments. All notes using custom instruments will be removed.')
         input('Press Enter to Continue')
 
-    new_song = pynbs.new_file()
-    new_song.header = song.header
-    new_song.layers = song.layers
-    new_song.header.tempo = 5
+    newSong = pynbs.new_file()
+    newSong.header = song.header
+    newSong.layers = song.layers
+    newSong.header.tempo = 5
 
-    has_max_chord_violation = False
+    hasMaxChordViolation = False
 
     # iterate through the whole song by chords
     for tick, chord in song:
-        new_tick = tick if not setting_compress else tick // 2
+        newTick = tick if not settingCompress else tick // 2
 
-        if new_tick > MAX_SONG_LENGTH:
+        if newTick > MAX_SONG_LENGTH:
             print('Notice: Your song was too long, so some had to be cut off the end.')
             break
 
-        if (tick % 2 != 0 and not setting_compress) or (tick % 2 == 0):
-            chord = remove_custom_notes(chord)
-            chord = fix_illegal_notes(chord)
-            chord, chord_violation = remove_chord_violations(chord)
+        if (tick % 2 != 0 and not settingCompress) or (tick % 2 == 0):
+            chord = removeCustomNotes(chord)
+            chord = fixIllegalNotes(chord)
+            chord, chordViolation = removeChordViolations(chord)
 
-            if chord_violation:
-                has_max_chord_violation = True
+            if chordViolation:
+                hasMaxChordViolation = True
 
             for note in chord:
-                note.tick = new_tick
+                note.tick = newTick
                 note.panning = 0
                 note.pitch = 0
-                new_song.notes.append(note)
+                newSong.notes.append(note)
 
-    if has_max_chord_violation:
+    if hasMaxChordViolation:
         print('Notice: Your song contained chords that were larger than allowed. Some notes were removed from these chords.')
 
     # save the new song
-    new_file_name = song_name + ' (Formatted).nbs'
+    newFileName = songName + ' (Formatted).nbs'
 
-    new_song.save(new_file_name)
-    print(f'Your formatted song was saved under "{new_file_name}"')
+    newSong.save(newFileName)
+    print(f'Your formatted song was saved under "{newFileName}"')
 
 
 if __name__ == '__main__':
--- nbs_generate_schematic.py
+++ nbs_generate_schematic.py
@@ -5,89 +5,88 @@ import mcschematic
 from constants import *
 
 
-def verify_format(song, song_file):
+def verifyFormat(song, songFile):
     print('Verifying your song...')
-    is_valid = True
+    isValid = True
     # check song length
     print('Checking song length...')
     if song.header.song_length > MAX_SONG_LENGTH:
         print('Warning: Your song is too long.')
-        is_valid = False
+        isValid = False
 
     # check custom instruments
     print('Checking for custom instruments...')
     if len(song.instruments) > 0:
         print('Warning: Your song contains custom instruments.')
-        is_valid = False
+        isValid = False
 
     # check range
     print('Checking note ranges...')
     for note in song.notes:
         if note.key < INSTRUMENT_RANGE[0] or note.key > INSTRUMENT_RANGE[1]:
             print('Warning: Your song contains notes that are outside the normal range.')
-            is_valid = False
+            isValid = False
             break
 
     # check chord lengths
     print('Checking chord lengths...')
     for tick, chord in song:
-        list_of_chords = {}
+        listOfChords = {}
         for note in chord:
-            if note.instrument in list_of_chords:
-                list_of_chords[note.instrument].append(note)
+            if note.instrument in listOfChords:
+                listOfChords[note.instrument].append(note)
             else:
-                list_of_chords[note.instrument] = [note]
-        for instrument, singleChord in list_of_chords.items():
-            lower_octave_notes = []
-            upper_octave_notes = []
+                listOfChords[note.instrument] = [note]
+        for instrument, singleChord in listOfChords.items():
+            lowerOctaveNotes = []
+            upperOctaveNotes = []
             for note in singleChord:
                 if note.key < INSTRUMENT_RANGE[0] + 12:
-                    lower_octave_notes.append(note)
+                    lowerOctaveNotes.append(note)
                 else:
-                    upper_octave_notes.append(note)
-            if len(lower_octave_notes) > CHORD_MAX_SIZES[INSTRUMENTS[instrument]] or len(upper_octave_notes) > \
-                    CHORD_MAX_SIZES[INSTRUMENTS[instrument]]:
+                    upperOctaveNotes.append(note)
+            if len(lowerOctaveNotes) > CHORD_MAX_SIZES[INSTRUMENTS[instrument]] or len(upperOctaveNotes) > CHORD_MAX_SIZES[INSTRUMENTS[instrument]]:
                 print('Warning: Your song contains chords that are larger than allowed.')
-                is_valid = False
+                isValid = False
                 break
-        if not is_valid:
+        if not isValid:
             break
 
-    if not is_valid:
+    if not isValid:
         input('We found some issues with your song. This is likely because you have not used the "nbs_format_song" script. Press Enter to automatically format your song.')
         import nbs_format_song
-        nbs_format_song.main(song_file)
+        nbs_format_song.main(songFile)
         print("Reverifying your song...")
-        verify_format(song, song_file)
+        verifyFormat(song, songFile)
     else:
         print('Song verified. Everything looks good!')
 
 
-def remove_empty_chests(chest_contents):
-    new_chest_contents = {}
-    for instrument, contents in chest_contents.items():
-        new_chest_contents[instrument] = []
+def removeEmptyChests(chestContents):
+    newChestContents = {}
+    for instrument, contents in chestContents.items():
+        newChestContents[instrument] = []
         for octaves in contents:
-            new_octaves = [[], []]
-            is_lower_octave_empty = 1
-            is_upper_octave_empty = 1
+            newOctaves = [[], []]
+            isLowerOctaveEmpty = 1
+            isUpperOctaveEmpty = 1
             for note in octaves[0]:
                 if note != -1:
-                    is_lower_octave_empty = 0
+                    isLowerOctaveEmpty = 0
                     break
             for note in octaves[1]:
                 if note != -1:
-                    is_upper_octave_empty = 0
+                    isUpperOctaveEmpty = 0
                     break
-            if is_lower_octave_empty == 0:
-                new_octaves[0] = octaves[0]
-            if is_upper_octave_empty == 0:
-                new_octaves[1] = octaves[1]
-            new_chest_contents[instrument].append(new_octaves)
-    return new_chest_contents
+            if isLowerOctaveEmpty == 0:
+                newOctaves[0] = octaves[0]
+            if isUpperOctaveEmpty == 0:
+                newOctaves[1] = octaves[1]
+            newChestContents[instrument].append(newOctaves)
+    return newChestContents
 
 
-def new_disc(slot, note):
+def newDisc(slot, note):
     if note == -1:
         return '{Count:1b,Slot:' + str(slot) + 'b,id:"minecraft:wooden_shovel"}'
     if note >= 12:
@@ -96,147 +95,147 @@ def new_disc(slot, note):
     return '{Count:1b,Slot:' + str(slot) + 'b,id:' + disc + '}'
 
 
-def create_shulker(current_shulker, contents):
-    slot = (current_shulker - 1) % 27
+def createShulker(currentShulker, contents):
+    slot = (currentShulker - 1) % 27
     # remove trailing comma
     contents = contents[:len(contents) - 1]
     return '{Count:1b,Slot:' + str(
         slot) + 'b,id:"minecraft:shulker_box",tag:{BlockEntityTag:{CustomName:\'{"text":"' + str(
-        current_shulker) + '"}\',Items:[' + contents + '],id:"minecraft:shulker_box"},display:{Name:\'{"text":"' + str(
-        current_shulker) + '"}\'}}}'
+        currentShulker) + '"}\',Items:[' + contents + '],id:"minecraft:shulker_box"},display:{Name:\'{"text":"' + str(
+        currentShulker) + '"}\'}}}'
 
 
-def create_chest(type_, contents):
+def createChest(type_, contents):
     # remove trailing comma
     if len(contents) > 0:
         contents = contents[:len(contents) - 1]
     return 'minecraft:chest[facing=south,type=' + type_ + ']{Items:[' + contents + ']}'
 
 
-def create_sign(instrument, current_module, octave):
-    octave_message = 'lower octave' if octave == 0 else 'upper octave'
+def createSign(instrument, currentModule, octave):
+    octaveMessage = 'lower octave' if octave == 0 else 'upper octave'
     return 'minecraft:oak_wall_sign[facing=south,waterlogged=false]{front_text:{color:"black",has_glowing_text:0b,messages:[\'{"text":"' + instrument + ' ' + str(
-        current_module) + '"}\',\'{"text":"' + octave_message + '"}\',\'{"text":""}\',\'{"text":""}\']},is_waxed:0b}'
+        currentModule) + '"}\',\'{"text":"' + octaveMessage + '"}\',\'{"text":""}\',\'{"text":""}\']},is_waxed:0b}'
 
 
 def main():
     # get song file from user
-    song_file = input('Please enter the file name of your song (include the .nbs): ')
-    if not song_file.endswith('.nbs'):
+    songFile = input('Please enter the file name of your song (include the .nbs): ')
+    if not songFile.endswith('.nbs'):
         sys.exit('Your song file must end with ".nbs".')
 
     try:
-        song = pynbs.read(song_file)
-        song_name = song_file[:-4]
+        song = pynbs.read(songFile)
+        songName = songFile[:-4]
     except Exception as e:
-        sys.exit(f'An error occurred while reading the song file "{song_file}".\nError name: {e.__class__.__name__}\nExact error (search this up for help): {e}')
+        sys.exit(f'An error occurred while reading the song file "{songFile}".\nError name: {e.__class__.__name__}\nExact error (search this up for help): {e}')
 
-    verify_format(song, song_file)
+    verifyFormat(song, songFile)
 
     # fix the length of the song for min fill of last chest
-    last_chest_fill = (song.header.song_length + 1) % 27
-    song_length_adjusted = song.header.song_length + 1
-    if 1 <= last_chest_fill < CHEST_MIN_FILL:
-        song_length_adjusted += CHEST_MIN_FILL - last_chest_fill
+    lastChestFill = (song.header.song_length + 1) % 27
+    songLengthAdjusted = song.header.song_length + 1
+    if 1 <= lastChestFill < CHEST_MIN_FILL:
+        songLengthAdjusted += CHEST_MIN_FILL - lastChestFill
 
     # initialize data structure
-    all_chest_contents = {}
-    empty_chest = numpy.full(song_length_adjusted, -1)
+    allChestContents = {}
+    emptyChest = numpy.full(songLengthAdjusted, -1)
     for instrument in INSTRUMENTS:
-        all_chest_contents[instrument] = []
+        allChestContents[instrument] = []
         for i in range(CHORD_MAX_SIZES[instrument]):
-            all_chest_contents[instrument].append([empty_chest.copy(), empty_chest.copy()])
+            allChestContents[instrument].append([emptyChest.copy(), emptyChest.copy()])
 
     # iterate through the whole song by chords
-    key_modifier = INSTRUMENT_RANGE[0]
-    current_indices = {}
+    keyModifier = INSTRUMENT_RANGE[0]
+    currentIndices = {}
     for tick, chord in song:
         # reset current indices
         for instrument in INSTRUMENTS:
-            current_indices[instrument] = [0, 0]
+            currentIndices[instrument] = [0, 0]
 
         for note in chord:
             instrument = INSTRUMENTS[note.instrument]
-            adjusted_key = note.key - key_modifier
-            octave = 0 if adjusted_key <= 11 else 1
-            all_chest_contents[instrument][current_indices[instrument][octave]][octave][tick] = adjusted_key
-            current_indices[instrument][octave] += 1
+            adjustedKey = note.key - keyModifier
+            octave = 0 if adjustedKey <= 11 else 1
+            allChestContents[instrument][currentIndices[instrument][octave]][octave][tick] = adjustedKey
+            currentIndices[instrument][octave] += 1
 
-    minimal_chest_contents = remove_empty_chests(all_chest_contents)
+    minimalChestContents = removeEmptyChests(allChestContents)
 
-    # turn minimal_chest_contents into a schematic
+    # turn minimalChestContents into a schematic
     schem = mcschematic.MCSchematic()
     offset = 0
     print('Generating Schematic...')
-    for instrument, contents in minimal_chest_contents.items():
-        current_module = 1
+    for instrument, contents in minimalChestContents.items():
+        currentModule = 1
         for module in contents:
-            lower_chest1 = ''
-            upper_chest1 = ''
-            lower_chest2 = ''
-            upper_chest2 = ''
-            lower_shulker = ''
-            upper_shulker = ''
-            current_shulker = 1
-            lower_octave_empty = len(module[0]) == 0
-            upper_octave_empty = len(module[1]) == 0
-            for currentTick in range(song_length_adjusted):
-                current_slot = currentTick % 27
-                if lower_octave_empty == 0:
-                    lower_shulker += new_disc(current_slot, module[0][currentTick]) + ','
-                if upper_octave_empty == 0:
-                    upper_shulker += new_disc(current_slot, module[1][currentTick]) + ','
+            lowerChest1 = ''
+            upperChest1 = ''
+            lowerChest2 = ''
+            upperChest2 = ''
+            lowerShulker = ''
+            upperShulker = ''
+            currentShulker = 1
+            lowerOctaveEmpty = len(module[0]) == 0
+            upperOctaveEmpty = len(module[1]) == 0
+            for currentTick in range(songLengthAdjusted):
+                currentSlot = currentTick % 27
+                if lowerOctaveEmpty == 0:
+                    lowerShulker += newDisc(currentSlot, module[0][currentTick]) + ','
+                if upperOctaveEmpty == 0:
+                    upperShulker += newDisc(currentSlot, module[1][currentTick]) + ','
                 # if we are on the last slot of a shulker box, or the song has ended
-                if (currentTick + 1) % 27 == 0 or currentTick == song_length_adjusted - 1:
+                if (currentTick + 1) % 27 == 0 or currentTick == songLengthAdjusted - 1:
                     # turn the shulker contents into actual shulker
-                    if lower_octave_empty == 0:
-                        lower_shulker = create_shulker(current_shulker, lower_shulker)
-                    if upper_octave_empty == 0:
-                        upper_shulker = create_shulker(current_shulker, upper_shulker)
+                    if lowerOctaveEmpty == 0:
+                        lowerShulker = createShulker(currentShulker, lowerShulker)
+                    if upperOctaveEmpty == 0:
+                        upperShulker = createShulker(currentShulker, upperShulker)
                     # if the current shulker should go in the first chests
-                    if current_shulker <= 27:
-                        if lower_octave_empty == 0:
-                            lower_chest1 += lower_shulker + ','
-                        if upper_octave_empty == 0:
-                            upper_chest1 += upper_shulker + ','
+                    if currentShulker <= 27:
+                        if lowerOctaveEmpty == 0:
+                            lowerChest1 += lowerShulker + ','
+                        if upperOctaveEmpty == 0:
+                            upperChest1 += upperShulker + ','
                     else:
-                        if lower_octave_empty == 0:
-                            lower_chest2 += lower_shulker + ','
-                        if upper_octave_empty == 0:
-                            upper_chest2 += upper_shulker + ','
+                        if lowerOctaveEmpty == 0:
+                            lowerChest2 += lowerShulker + ','
+                        if upperOctaveEmpty == 0:
+                            upperChest2 += upperShulker + ','
                     # reset the shulkers and increment the current shulker
-                    lower_shulker = ''
-                    upper_shulker = ''
-                    current_shulker += 1
+                    lowerShulker = ''
+                    upperShulker = ''
+                    currentShulker += 1
 
-            if lower_octave_empty == 0:
-                lower_chest1 = create_chest('right', lower_chest1)
-                lower_chest2 = create_chest('left', lower_chest2)
-                schem.setBlock((offset, 0, -1), lower_chest1)
-                schem.setBlock((offset + 1, 0, -1), lower_chest2)
-                schem.setBlock((offset, 0, 0), create_sign(instrument, current_module, 0))
+            if lowerOctaveEmpty == 0:
+                lowerChest1 = createChest('right', lowerChest1)
+                lowerChest2 = createChest('left', lowerChest2)
+                schem.setBlock((offset, 0, -1), lowerChest1)
+                schem.setBlock((offset + 1, 0, -1), lowerChest2)
+                schem.setBlock((offset, 0, 0), createSign(instrument, currentModule, 0))
             else:
                 schem.setBlock((offset, 0, -1), "minecraft:air")
                 schem.setBlock((offset + 1, 0, -1), "minecraft:air")
                 schem.setBlock((offset, 0, 0), "minecraft:air")
 
-            if upper_octave_empty == 0:
-                upper_chest1 = create_chest('right', upper_chest1)
-                upper_chest2 = create_chest('left', upper_chest2)
-                schem.setBlock((offset, 1, -1), upper_chest1)
-                schem.setBlock((offset + 1, 1, -1), upper_chest2)
-                schem.setBlock((offset, 1, 0), create_sign(instrument, current_module, 1))
+            if upperOctaveEmpty == 0:
+                upperChest1 = createChest('right', upperChest1)
+                upperChest2 = createChest('left', upperChest2)
+                schem.setBlock((offset, 1, -1), upperChest1)
+                schem.setBlock((offset + 1, 1, -1), upperChest2)
+                schem.setBlock((offset, 1, 0), createSign(instrument, currentModule, 1))
             else:
                 schem.setBlock((offset, 1, -1), "minecraft:air")
                 schem.setBlock((offset + 1, 1, -1), "minecraft:air")
                 schem.setBlock((offset, 1, 0), "minecraft:air")
 
-            current_module += 1
+            currentModule += 1
             offset += 2
 
-    save_name = song_name.lower().replace('(', '').replace(')', '').replace(' ', '_')
-    schem.save('', save_name, mcschematic.Version.JE_1_20)
-    print('Your schematic was successfully generated and saved under "' + save_name + '.schem"')
+    saveName = songName.lower().replace('(', '').replace(')', '').replace(' ', '_')
+    schem.save('', saveName, mcschematic.Version.JE_1_20)
+    print('Your schematic was successfully generated and saved under "' + saveName + '.schem"')
 
 
 if __name__ == '__main__':

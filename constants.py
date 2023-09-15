4510db1eec27c976fc5de03e7b6c36c1e8392f54
IHasBone <89635515+IHasBone@users.noreply.github.com>
2023-09-15 08:02:52 +0200

    Various improvements (#10)
    * Add .idea in .gitignore to exclude files generated by JetBrains
    
    * Format python files to align with PEP 8 (spacing, naming, exceptions, etc.)
    
    * Improve nbs_format_song.py
    In the original version, when getting the lower/upper octave notes in the remove highest notes (unlike in the remove lowest notes), they would both be the exactly the same, since they were defined as lowest = highest = [].
    However, in the remove lowest notes, they were defined as lower = [], highest = [], so that they were not linked.
    I have kept the original loop for the remove highest notes, in case it is for some reason needed.
    
    * Made int -> boolean where possible in verify_format
    
    * Simplify chained expression
    
    * Improve README clarity
    
    * nbs_generate_schematic now calls nbs_format_song if the song is not already formatted (nbs_format_song still works by itself)
    
    * When the program exits with an error, it now displays the name of the error
    
    * Went back to old naming scheme
    
    * fixed the remove highest chord bug described in an earlier commit (which was left in then)
    
    * The schematic script no longer calls the format script
    
    * Change indent to two spaces, instead of four
    
    * Everything that can use booleans does
    
    * Fix typos in README
    
    * Improve nbs_generate_schematic

--- constants.py
+++ constants.py
@@ -2,51 +2,54 @@
 # Choose whether or not to use named discs
 # Using unnamed discs will reduce schematic size and
 # Possibly reduce lag due to less NBT data
-NAME_DISCS = 0
+# Options: True, False (it's case-sensitive,
+# so make sure it's written with a capital first letter and
+# all other letters lowercase)
+NAME_DISCS = False
 
 # EDITABLE SETTING
 # The number of full modules (2 half-modules) of each instrument
 # in the physical machine. Can be set to 0 if the module does not exist.
 # NOTE: These settings are configured for the machine in the world download.
 CHORD_MAX_SIZES = {
-    'piano': 3,
-    'double bass': 1,
-    'bass drum': 1,
-    'snare drum': 1,
-    'click': 1,
-    'guitar': 3,
-    'flute': 3,
-    'bell': 1,
-    'chime': 1,
-    'xylophone': 1,
-    'iron xylophone': 1,
-    'cow bell': 1,
-    'digeridoo': 1,
-    'bit': 1,
-    'banjo': 1,
-    'pling': 3,
+  'piano': 3,
+  'double bass': 1,
+  'bass drum': 1,
+  'snare drum': 1,
+  'click': 1,
+  'guitar': 3,
+  'flute': 3,
+  'bell': 1,
+  'chime': 1,
+  'xylophone': 1,
+  'iron xylophone': 1,
+  'cow bell': 1,
+  'digeridoo': 1,
+  'bit': 1,
+  'banjo': 1,
+  'pling': 3,
 }
 
 # EDITABLE SETTING
 # Whether you want to keep the lowest or highest notes in chords that are too large
 # 'l' means keep the low notes; 'h' means keep the high notes.
 KEEP_NOTES_BY_INSTRUMENT = {
-    'piano': 'h',
-    'double bass': 'l',
-    'bass drum': 'l',
-    'snare drum': 'l',
-    'click': 'l',
-    'guitar': 'l',
-    'flute': 'h',
-    'bell': 'l',
-    'chime': 'l',
-    'xylophone': 'l',
-    'iron xylophone': 'l',
-    'cow bell': 'l',
-    'digeridoo': 'l',
-    'bit': 'l',
-    'banjo': 'l',
-    'pling': 'h',
+  'piano': 'h',
+  'double bass': 'l',
+  'bass drum': 'l',
+  'snare drum': 'l',
+  'click': 'l',
+  'guitar': 'l',
+  'flute': 'h',
+  'bell': 'l',
+  'chime': 'l',
+  'xylophone': 'l',
+  'iron xylophone': 'l',
+  'cow bell': 'l',
+  'digeridoo': 'l',
+  'bit': 'l',
+  'banjo': 'l',
+  'pling': 'h',
 }
 
 # DO NOT EDIT
@@ -60,22 +63,22 @@ CHEST_MIN_FILL = 4
 # DO NOT EDIT
 # The instrument names in order
 INSTRUMENTS = [
-    'piano',
-    'double bass',
-    'bass drum',
-    'snare drum',
-    'click',
-    'guitar',
-    'flute',
-    'bell',
-    'chime',
-    'xylophone',
-    'iron xylophone',
-    'cow bell',
-    'digeridoo',
-    'bit',
-    'banjo',
-    'pling',
+  'piano',
+  'double bass',
+  'bass drum',
+  'snare drum',
+  'click',
+  'guitar',
+  'flute',
+  'bell',
+  'chime',
+  'xylophone',
+  'iron xylophone',
+  'cow bell',
+  'digeridoo',
+  'bit',
+  'banjo',
+  'pling',
 ]
 
 # DO NOT EDIT
@@ -85,35 +88,35 @@ INSTRUMENT_RANGE = (33, 57)
 # DO NOT EDIT
 # List of music discs in order
 NOTES_TO_DISCS_UNNAMED = [
-    '"minecraft:music_disc_13"',
-    '"minecraft:music_disc_cat"',
-    '"minecraft:music_disc_blocks"',
-    '"minecraft:music_disc_chirp"',
-    '"minecraft:music_disc_far"',
-    '"minecraft:music_disc_mall"',
-    '"minecraft:music_disc_mellohi"',
-    '"minecraft:music_disc_stal"',
-    '"minecraft:music_disc_strad"',
-    '"minecraft:music_disc_ward"',
-    '"minecraft:music_disc_11"',
-    '"minecraft:music_disc_wait"',
-    '"minecraft:music_disc_pigstep"',
+  '"minecraft:music_disc_13"',
+  '"minecraft:music_disc_cat"',
+  '"minecraft:music_disc_blocks"',
+  '"minecraft:music_disc_chirp"',
+  '"minecraft:music_disc_far"',
+  '"minecraft:music_disc_mall"',
+  '"minecraft:music_disc_mellohi"',
+  '"minecraft:music_disc_stal"',
+  '"minecraft:music_disc_strad"',
+  '"minecraft:music_disc_ward"',
+  '"minecraft:music_disc_11"',
+  '"minecraft:music_disc_wait"',
+  '"minecraft:music_disc_pigstep"',
 ]
 
 # DO NOT EDIT
 # List of music discs in order, but named
 NOTES_TO_DISCS_NAMED = [
-    '"minecraft:music_disc_13",tag:{display:{Name:\'{"text":"F#"}\'}}',
-    '"minecraft:music_disc_cat",tag:{display:{Name:\'{"text":"G"}\'}}',
-    '"minecraft:music_disc_blocks",tag:{display:{Name:\'{"text":"G#"}\'}}',
-    '"minecraft:music_disc_chirp",tag:{display:{Name:\'{"text":"A"}\'}}',
-    '"minecraft:music_disc_far",tag:{display:{Name:\'{"text":"A#"}\'}}',
-    '"minecraft:music_disc_mall",tag:{display:{Name:\'{"text":"B"}\'}}',
-    '"minecraft:music_disc_mellohi",tag:{display:{Name:\'{"text":"C"}\'}}',
-    '"minecraft:music_disc_stal",tag:{display:{Name:\'{"text":"C#"}\'}}',
-    '"minecraft:music_disc_strad",tag:{display:{Name:\'{"text":"D"}\'}}',
-    '"minecraft:music_disc_ward",tag:{display:{Name:\'{"text":"D#"}\'}}',
-    '"minecraft:music_disc_11",tag:{display:{Name:\'{"text":"E"}\'}}',
-    '"minecraft:music_disc_wait",tag:{display:{Name:\'{"text":"F"}\'}}',
-    '"minecraft:music_disc_pigstep",tag:{display:{Name:\'{"text":"F#"}\'}}',
+  '"minecraft:music_disc_13",tag:{display:{Name:\'{"text":"F#"}\'}}',
+  '"minecraft:music_disc_cat",tag:{display:{Name:\'{"text":"G"}\'}}',
+  '"minecraft:music_disc_blocks",tag:{display:{Name:\'{"text":"G#"}\'}}',
+  '"minecraft:music_disc_chirp",tag:{display:{Name:\'{"text":"A"}\'}}',
+  '"minecraft:music_disc_far",tag:{display:{Name:\'{"text":"A#"}\'}}',
+  '"minecraft:music_disc_mall",tag:{display:{Name:\'{"text":"B"}\'}}',
+  '"minecraft:music_disc_mellohi",tag:{display:{Name:\'{"text":"C"}\'}}',
+  '"minecraft:music_disc_stal",tag:{display:{Name:\'{"text":"C#"}\'}}',
+  '"minecraft:music_disc_strad",tag:{display:{Name:\'{"text":"D"}\'}}',
+  '"minecraft:music_disc_ward",tag:{display:{Name:\'{"text":"D#"}\'}}',
+  '"minecraft:music_disc_11",tag:{display:{Name:\'{"text":"E"}\'}}',
+  '"minecraft:music_disc_wait",tag:{display:{Name:\'{"text":"F"}\'}}',
+  '"minecraft:music_disc_pigstep",tag:{display:{Name:\'{"text":"F#"}\'}}',
 ]
